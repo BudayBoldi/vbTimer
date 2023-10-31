1aa8f8085075c81ece5b0c122eca9ee985c830dc
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2023-10-31 15:07:05 +0100

    timeout using args

--- Program.vb
+++ Program.vb
@@ -4,12 +4,10 @@ Module Program
     Public Sub Main(args As String())
         Dim counter As Integer = 0
 
-        Do Until counter = 15
+        Do Until counter = Convert.ToInt32(args)
             counter = counter + 1
             System.Threading.Thread.Sleep(1000)
             System.Console.WriteLine(counter)
         Loop
-
-        Console.ReadKey()
     End Sub
 End Module
